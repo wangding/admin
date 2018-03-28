80a2b8f3b0b1bb7385e8728f240402ea09682a02
wangding <408542507@qq.com>
2018-03-28 17:49:50 +0800

    feat: 实现了菜单组件

--- js/c-menu.js
+++ js/c-menu.js
@@ -1,8 +1,101 @@
 var $menu = function() {
+  var $menuDOM = (function(menuData) {
+    var $menu = $(''
+      + '<div class="admin-app-menu">'
+        + '<ul class="menu"></ul>'
+      + '</div>');
+
+    var $ul = $menu.find('.menu');
+
+    menuData.forEach(function(e) {
+      var $li = $('<li></li>');
+
+      var $menuBar = $(''
+        + '<div class="menu-bar">'
+          + '<i class="iconfont ' + e.icon + '"></i>'
+          + '<span>' + e.title + '</span>'
+          + '<i class="iconfont icon-arrowdown"></i>'
+        + '</div>');
+
+      $li.append($menuBar);
+
+      var $menuItems = $('<dl class="menu-items"></dl>');
+
+      e.items.forEach(function(item) {
+        var $item = $(''
+          + '<dd class="menu-item" data-href="' + item.url + '">' 
+            + item.title
+          + '</dd>');
+
+        $menuItems.append($item);
+      });
+
+      $li.append($menuItems);
+      $ul.append($li);
+    });
+
+    return $menu;
+  })(app.menuData);    
+
+  var $menuBar = $menuDOM.find('.menu-bar'),
+    $menuItem = $menuDOM.find('.menu-item'),
+    $currentMenuBar = null,
+    $currentMenu = null;
+
+  $menuBar.click(function(e) {
+    var $menu = $(e.currentTarget),
+      arrowStyle = {'up': 'icon-arrowup', 'down': 'icon-arrowdown'};
+
+    $menu.showArrow = function(style) {
+      var $arrow = this.find('span+i');
+
+      var cssClass = $arrow.hasClass(arrowStyle.up) ? arrowStyle.up : arrowStyle.down;
+      $arrow.removeClass(cssClass);
+
+      $arrow.addClass(style);
+    };
+
+    if($currentMenuBar === null) {
+      $menu.showArrow(arrowStyle.up);
+      $menu.next().css('display', 'block');
+      $currentMenuBar = $menu;
+    } else if($currentMenuBar.get(0) === $menu.get(0)) {
+      $menu.showArrow(arrowStyle.down);
+      $menu.next().css('display', 'none');
+      $currentMenuBar = null;
+    } else {
+      $currentMenuBar.showArrow(arrowStyle.down);
+      $currentMenuBar.next().css('display', 'none');
+      $menu.showArrow(arrowStyle.up);
+      $menu.next().css('display', 'block');
+      $currentMenuBar = $menu;
+    }
+  });
+
+  $menuItem.click(function(e) {
+    var $menuItem = $(e.currentTarget);
+    
+    $menuItem.selectMenu = function(isSelected){
+      if(isSelected) {
+        this.css({'background': '#009688', 'color': 'white'});
+      } else {
+        this.css({'background': '#282e37', 'color': '#c2c2c2'});
+      }
+    };
+
+    if($currentMenu === null) {
+      $menuItem.selectMenu(true);
+      $currentMenu = $menuItem;
+    } else {
+      $currentMenu.selectMenu(false);
+      $menuItem.selectMenu(true);
+      $currentMenu = $menuItem;
+    }
+  });
+
   function show(cfg) {
-    $(cfg.container).append($('<div class="admin-app-menu"></div>'));
+    $(cfg.container).append($menuDOM);
   }
   
   return {show: show};
 }();
-
